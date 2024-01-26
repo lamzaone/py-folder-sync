8d106b9124fdc438ba1a5f5cf24632b3d8c23e1c
lamzaone <denisdenis009@gmail.com>
2024-01-26 15:34:01 +0200

    - Improved the structure - Added comments - added launch.json and tasks.json
    FIXED:
    - LOCAL_FOLDER and REMOTE_FOLDER were not being fetched correctly

--- local/client.py
+++ local/client.py
@@ -3,27 +3,26 @@ import socket
 import os
 
 
-PORT = 8080
-BUFFER = 1024
-LOCAL_FOLDER = os.getcwd() # the folder where the files are stored on the client machine (remote), current folder in this case
-SLEEP_TIME = 5 # seconds
+PORT = 8080     # the port number on which the server is listening
+BUFFER = 1024   # the size of the buffer (1 KB)
+LOCAL_FOLDER = os.path.dirname(os.path.abspath(__file__))   # the folder where the files are stored on the server machine (remote) 
+SLEEP_TIME = 5  # seconds
 
 
 
 
 
 def sendFile(s, file):
-    with open(file, 'rb') as f:
-        data = f.read(BUFFER)
-        while data:            
-            print(data) 
-            s.sendall(data)
-            if s.recv(BUFFER).decode() != 'OK':
-                print("Error:", s.recv(BUFFER).decode())
-                break
-            data = f.read(BUFFER)
-    s.sendall("EOF".encode())
-    message = s.recv(BUFFER).decode()
+    with open(os.path.join(LOCAL_FOLDER, file), 'rb') as f:  # open the file in binary reading mode
+        data = f.read(BUFFER)    # read the first BUFFER sized chunk of data
+        while data:     # loop to read the rest of the data and send it 
+            s.sendall(data)     # send the data
+            if s.recv(BUFFER).decode() != 'OK':      # wait for a confirmation message from the server, to make sure that the data is received
+                print("Error:", s.recv(BUFFER).decode())    # if the confirmation message is not received, print the error message
+                break 
+            data = f.read(BUFFER)   #  read the next chunk of data, f.read() returns an empty string after reading the whole file
+    s.sendall("EOF".encode())       # send a message to the server to let it know that the end of the file is reached
+    message = s.recv(BUFFER).decode()       # wait for a confirmation message from the server, to make sure that the file is received
     if message == 'received':
         print("File", file, "sent")
     else:  
@@ -31,42 +30,67 @@ def sendFile(s, file):
 
 def synchroniseFiles(s, folder):
     # send the list of files in the folder
-    files = os.listdir(folder)
+    files = os.listdir(folder)      # get the list of files in the folder
     for file in files:
         print("File:", file)
-        if file == 'client.py':
-            continue
-        s.sendall(file.encode())
-        response = s.recv(BUFFER).decode()
-        if response == 'not found':
+        if file == 'client.py':     # skip this file
+            continue                # skip the rest of the code in the loop and go to the next iteration
+        s.sendall(file.encode())        # send the name of the file
+        response = s.recv(BUFFER).decode()      # wait for a response from the server
+        if response == 'not found':     # if the file is not found on the server, send it
             sendFile(s, file)
-        elif response == 'found':
+        elif response == 'found':       # if the file is found on the server, skip it
             continue
         else:
             print("Error:", response)
             break
-    s.sendall('EOL'.encode())
+    s.sendall('EOL'.encode())      # send a message to the server to let it know that the end of the list is reached
 
 
 
 
 
+def connect(try_number = 1):
+    global s
+    try:
+        s.connect(('localhost', PORT)) # connect to the server
+        if s is not None: # if the connection is successful, print the address of the server
+            print('Connected to', s.getpeername()) # getpeername() returns the address of the remote endpoint
+    except ConnectionRefusedError: # if the connection is refused, try again
+        if try_number > 5: # if the connection is refused 5 times, exit the application
+            print("Connection failed, closing application...")
+            exit()
+        print(f"[{try_number}/5] Connection refused, trying again...") # print the number of tries
+        connect(try_number + 1) # try again and increment the number of tries
 
 
 
 
-
-# create a socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET: IPv4, SOCK_STREAM: TCP
-# connect to the server
-s.connect(('localhost', PORT))
-if s is not None:
-    print('Connected to', s.getpeername())
 
-while s is not None:
-    synchroniseFiles(s, LOCAL_FOLDER)
-    print('Waiting', SLEEP_TIME, 'seconds...')
-    sleep(SLEEP_TIME)
+def main():
+    global s
+    connect() # connect to the server using the connect() function defined above
 
-# close the socket
-s.close()
+    try:
+        while True:
+            synchroniseFiles(s, LOCAL_FOLDER)
+            print('Waiting', SLEEP_TIME, 'seconds...')
+            sleep(SLEEP_TIME)
+    except ConnectionAbortedError:
+        print("Connection aborted")
+    except ConnectionResetError:
+        print("Connection reset")
+        connect()
+    except KeyboardInterrupt:
+        print("Keyboard interrupt")
+    except Exception as e:
+        print("Error:", e)
+        exit()
+
+    print("Closing connection...")
+    s.close()
+
+
+if __name__ == "__main__":
+    main()
--- remote/server.py
+++ remote/server.py
@@ -4,37 +4,35 @@ from time import sleep
 
 PORT = 8080
 BUFFER = 1024
-REMOTE_FOLDER = os.getcwd() # the folder where the files are stored on the server machine (remote), current folder in this case 
+REMOTE_FOLDER = os.path.dirname(os.path.abspath(__file__)) # the folder where the files are stored on the server machine (remote) 
 SLEEP_TIME = 5 # seconds
 
 
 def recieveFile(client_socket, file):
-    with open(file, 'wb') as f: 
-        data = client_socket.recv(BUFFER)
-        while data:
-            print(data)
-            if data.decode() == "EOF":
-                break
-            f.write(data)
-            client_socket.sendall('OK'.encode())
-            data = client_socket.recv(BUFFER)
+    with open(os.path.join(REMOTE_FOLDER, file), 'wb') as f: # open the file in binary writing mode
+        data = client_socket.recv(BUFFER) # receive the first chunk of data
+        while data: # loop to write in file and receive the rest of the data
+            if data.decode() == "EOF": # if the end of the file is reached, break
+                break 
+            f.write(data) # write the data to the file
+            client_socket.sendall('OK'.encode()) # send a confirmation message to the client
+            data = client_socket.recv(BUFFER) # receive the next chunk of data
+
     print("File", file, "recieved")
-    client_socket.sendall('received'.encode())
+    client_socket.sendall('received'.encode()) # send a confirmation message to the client
 
 
 def synchroniseFiles(client_socket, folder):
     while True:
-        file = client_socket.recv(BUFFER).decode()
-        if file == 'EOL':
+        file = client_socket.recv(BUFFER).decode() # receive the name of the file
+        if file == 'EOL': # check if the end of the list is reached
             break
-        print("File:", file)
-        if file not in os.listdir(folder):
-            client_socket.sendall('not found'.encode())
-            recieveFile(client_socket, file)
-            continue
+        print("File:", file) # print the name of the file
+        if file not in os.listdir(folder): # check if the file is not in the folder
+            client_socket.sendall('not found'.encode()) # send a message to the client to let it know that the file is not found on the server, so it should send it
+            recieveFile(client_socket, file) # receive the file
         else:
-            client_socket.sendall('found'.encode())
-
+            client_socket.sendall('found'.encode()) # send a message to the client to let it know that the file is found on the server, so it should not send it
 
 
 
@@ -42,14 +40,28 @@ def synchroniseFiles(client_socket, folder):
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET: IPv4, SOCK_STREAM: TCP
-s.bind(('', PORT)) # '' means all available interfaces on the host machine (server)
-s.listen(5) # 5 is the maximum number of queued connections
-client_socket, client_address = s.accept()
-print('Connected to', client_address)
+def main():
+    global s    
 
-while client_socket:
-    synchroniseFiles(client_socket, REMOTE_FOLDER)
-    print("Waiting for" , SLEEP_TIME, "seconds...")
-    sleep(SLEEP_TIME)  
 
-s.close()
+    try:
+        s.bind(('', PORT)) # '' means all available interfaces on the host machine (server)
+        s.listen(5) # 5 is the maximum number of queued connections
+        client_socket, client_address = s.accept()
+        print('Connected to', client_address)
+        while client_socket:
+            synchroniseFiles(client_socket, REMOTE_FOLDER)
+            print("Waiting for" , SLEEP_TIME, "seconds...")
+            sleep(SLEEP_TIME)
+    except KeyboardInterrupt:
+        print("Closing the connection...")
+        client_socket.close()
+        exit()
+    except Exception as e:
+        print("Error:", e)
+        exit()
+
+    s.close()
+
+if __name__ == "__main__": 
+    main()
